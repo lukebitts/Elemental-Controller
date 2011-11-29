ada957c2942140ee0262123f272f3549e728fb56
Lucas <lucas@asa.tc>
2011-11-29 13:35:34 -0200

    Physics changes (gotta fix them)

--- js/elemental-controller.js
+++ js/elemental-controller.js
@@ -13,22 +13,69 @@ Crafty.c("TilePhysics",{
 
 Crafty.c("CharacterPhysics",{
 	gravity:9.8,
-	force:{x:0,y:1},
+	force:{x:0,y:0},
 	floor:false,
 	init:function(){
 		this.requires("2D, Collision")
 			.bind("EnterFrame",function(){
 				this.force.y += this.gravity/10;
+				this.force.y = this.floor?0:this.force.y;
 				this.force.x *= 0.9;
+				this.force.x = this.force.x<0.2&&this.force.x>-0.2?0:this.force.x;
 				this.x += this.force.x;
 				this.y += this.force.y;
 				if(!this.hit("TILE")) this.floor = false;
 			})
 			.onHit("TILE",function(e){
 				this.floor = true;
-				var obj = e[0].obj;
+				for(i in e){
+					var dir = []; //Where it collided = TOP, BOT, LEF, RIG, TOPLEF, TOPRIG, BOTLEF, BOTRIG
+					var from = undefined; //Direction before collision = TOPBOT, LEFRIG
+					var obj = e[i].obj;
+					
+					/*if(this.x <= obj.x) dir = "LEF";
+					if(this.x+this.w >= obj.x+obj.w) dir = "RIG";
+					
+					if(this.y <= obj.y) dir = "TOP";
+					if(this.y+this.h >= obj.y+obj.h) dir = "BOT";*/
+					
+					if(this.force.x > 0) dir.x = "LEF";
+					if(this.force.x < 0) dir.x = "RIG";
+					if(this.force.x == 0) dir.x = false;
+					
+					if(this.force.y > 0) dir.y = "TOP";
+					if(this.force.y < 0) dir.y = "BOT";
+					if(this.force.y == 0) dir.x = false;
+					
+					
+					//$(dir);
+					
+					//if(!dir.x){
+						if(dir.y == "TOP"){
+							this.force.y = 0;
+							this.y = obj.y - this.h;
+						}
+						if(dir.y == "BOT"){
+							this.force.y = 0;
+							this.y = obj.y + obj.h;
+						}
+					//}
+					if(!dir.y){
+						if(dir.x == "LEF"){
+							this.force.x = 0;
+							this.x = obj.x - this.w;
+						}
+						if(dir.x == "RIG"){
+							this.force.x = 0;
+							this.x = obj.x + obj.w;
+						}
+					}
+					
+				}
+				/*var obj = e[0].obj;
 				this.force.y = 0;
-				this.y = obj.y - this.h;
+				this.y = obj.y - this.h;*/
+				
 			})
 	}
 })
@@ -54,8 +101,10 @@ Crafty.c("PlayerControl",{
 				if(this.keys[65])
 					if(this.force.x > -5)
 						this.force.x -= 0.5;
-				if(this.keys[87] && this.floor)
+				if(this.keys[87] && this.floor){
+					this.floor = false;
 					this.force.y = -13;
+				}
 			})
 	}
 })
@@ -84,5 +133,5 @@ window.onload = function(){
 	createTile({x:300,y:450},"DIRT");
 	
 	
-	createPlayer({x:160,y:20});
+	createPlayer({x:360,y:440});
 }
