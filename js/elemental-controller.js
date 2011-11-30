aee1744fe3b0ad27404211d9cf21b7717581117a
Lucas Bittencourt <lbitts@hotmail.com>
2011-11-30 15:19:11 -0200

    physics changes

--- js/elemental-controller.js
+++ js/elemental-controller.js
@@ -1,137 +1,102 @@
-function $(msg){
-	console.log(msg);
-}
-
-Crafty.c("TilePhysics",{
+Crafty.c("Box2DWorld",{
+	debugging:true,
+	world:undefined,
 	init:function(){
-		this.requires("2D, Collision")
-			.onHit("PLAYER",function(){
-			//$("hitou");
+		this.requires("2D, Canvas")
+		var worldAABB = new b2AABB();
+		worldAABB.minVertex.Set(0, 0);
+		worldAABB.maxVertex.Set(800, 600);
+		var gravity = new b2Vec2(0, 300);
+		this.world = new b2World(worldAABB, gravity, true);
+		this.bind("EnterFrame",function(){
+			var timeStep = 1.0/60;
+			var iteration = 1;
+			this.world.Step(timeStep, iteration);
 		})
 	}
-})
+});
 
-Crafty.c("CharacterPhysics",{
-	gravity:9.8,
-	force:{x:0,y:0},
-	floor:false,
+Crafty.c("PhysicsModel",{
+	_physicsModel:undefined,
+	_world:Crafty(Crafty("Box2DWorld")[0]).world,
+	physicsModel:function(obj){
+		this._physicsModel = obj;
+		this.trigger("PhysicsModelChange");
+		return this;
+	}
+});
+
+Crafty.c("PhysicsEnabled",{
 	init:function(){
-		this.requires("2D, Collision")
+		this.requires("2D, PhysicsModel")
+			.bind("PhysicsModelChange",function(){
+				//console.log("Modelo mudado");
+			})
 			.bind("EnterFrame",function(){
-				this.force.y += this.gravity/10;
-				this.force.y = this.floor?0:this.force.y;
-				this.force.x *= 0.9;
-				this.force.x = this.force.x<0.2&&this.force.x>-0.2?0:this.force.x;
-				this.x += this.force.x;
-				this.y += this.force.y;
-				if(!this.hit("TILE")) this.floor = false;
-			})
-			.onHit("TILE",function(e){
-				this.floor = true;
-				for(i in e){
-					var dir = []; //Where it collided = TOP, BOT, LEF, RIG, TOPLEF, TOPRIG, BOTLEF, BOTRIG
-					var from = undefined; //Direction before collision = TOPBOT, LEFRIG
-					var obj = e[i].obj;
-					
-					/*if(this.x <= obj.x) dir = "LEF";
-					if(this.x+this.w >= obj.x+obj.w) dir = "RIG";
-					
-					if(this.y <= obj.y) dir = "TOP";
-					if(this.y+this.h >= obj.y+obj.h) dir = "BOT";*/
-					
-					if(this.force.x > 0) dir.x = "LEF";
-					if(this.force.x < 0) dir.x = "RIG";
-					if(this.force.x == 0) dir.x = false;
-					
-					if(this.force.y > 0) dir.y = "TOP";
-					if(this.force.y < 0) dir.y = "BOT";
-					if(this.force.y == 0) dir.x = false;
-					
-					
-					//$(dir);
-					
-					//if(!dir.x){
-						if(dir.y == "TOP"){
-							this.force.y = 0;
-							this.y = obj.y - this.h;
-						}
-						if(dir.y == "BOT"){
-							this.force.y = 0;
-							this.y = obj.y + obj.h;
-						}
-					//}
-					if(!dir.y){
-						if(dir.x == "LEF"){
-							this.force.x = 0;
-							this.x = obj.x - this.w;
-						}
-						if(dir.x == "RIG"){
-							this.force.x = 0;
-							this.x = obj.x + obj.w;
-						}
-					}
-					
-				}
-				/*var obj = e[0].obj;
-				this.force.y = 0;
-				this.y = obj.y - this.h;*/
-				
-			})
+				if(this._physicsModel == undefined) return;
+				//@TODO: Fix the rotation from the physics to the graphics
+				//this._physicsModel.m_rotation = 0;
+				this.rotation = this._physicsModel.m_rotation*180/Math.PI;
+				this.x = this._physicsModel.m_position.x  - this.w/2;
+				this.y = this._physicsModel.m_position.y  - this.h/2;
+			});
 	}
 })
 
-Crafty.c("PlayerControl",{
-	keys:[],
-	init:function(){
-		this.requires("CharacterPhysics, Keyboard")
-			.bind("KeyDown",function(e){
-				this.keys[e.key] = true;
-			})
-			.bind("KeyUp",function(e){
-				this.keys[e.key] = false;
-				if(e.key == 68 && this.floor)
-					this.force.x = 0.5;
-				if(e.key == 65 && this.floor)
-					this.force.x = -0.5;
-			})
-			.bind("EnterFrame",function(){
-				if(this.keys[68])
-					if(this.force.x < 5)
-						this.force.x += 0.5;
-				if(this.keys[65])
-					if(this.force.x > -5)
-						this.force.x -= 0.5;
-				if(this.keys[87] && this.floor){
-					this.floor = false;
-					this.force.y = -13;
-				}
-			})
-	}
-})
-
-function createTile(position, args){
-	return Crafty.e("TILE")
-		.addComponent("2D, Canvas, TilePhysics, Color"+(args?", "+args:""))
-		.attr({x:position.x,y:position.y,w:50,h:50})
-		.color("#733D1A")
-}
-
-function createPlayer(position, args){
-	return Crafty.e("PLAYER")
-		.addComponent("2D, Canvas, CharacterPhysics, PlayerControl, Color"+(args?", "+args:""))
-		.attr({x:position.x,y:position.y,w:10,h:10})
-		.color("#733D1A")
+function createSimpleRigidBody(world,x,y,w,h,density){
+	var boxSd = new b2BoxDef();
+	boxSd.density = density==undefined?1:density; 
+	boxSd.restitution = 0.2;
+	boxSd.friction = 0.8;
+	boxSd.extents.Set(w/2, h/2);
+	var boxBd = new b2BodyDef();
+	boxBd.AddShape(boxSd);
+	boxBd.position.Set(x,y);
+	var boxr = world.CreateBody(boxBd);
+	
+	var r = Crafty.e("Canvas, PhysicsEnabled")
+		.attr({w:w,h:h})
+		.physicsModel(boxr);
+		
+	return r;
 }
 
 window.onload = function(){
 	Crafty.init(800,600);
 	Crafty.canvas.init();
 	
-	for(var i = 0;i<10; i++){
-		createTile({x:100+50*i,y:500},"DIRT");
-	}
-	createTile({x:300,y:450},"DIRT");
+	Crafty.e("2D, Canvas, Color")
+		.attr({w:800,h:600})
+		.color("#000");
 	
+	//Create the World;
+	var world = Crafty.e("Box2DWorld");
 	
-	createPlayer({x:360,y:440});
+	console.log(world.world);
+	
+	createSimpleRigidBody(world.world,50,50,40,40,0.1)
+		.addComponent("Keyboard, Color")
+		.color("#0af")
+		.bind("KeyDown",function(e){
+			console.log(e.key);
+			this.keys[e.key] = true;
+		})
+		.bind("KeyUp",function(e){
+			this.keys[e.key] = false;
+		})
+		.bind("EnterFrame",function(){
+			if(!this.keys) this.keys = [];
+			if(this.keys[68]){
+				//this._physicsModel.ApplyForce(new b2Vec2(0,-500),new b2Vec2(20,20));
+				//this._physicsModel.ApplyTorque(450);
+				this._physicsModel.ApplyForce(new b2Vec2(-100,-300),new b2Vec2(0,0));
+			}
+		})
+	createSimpleRigidBody(world.world,0,200,800,40,0)
+		.addComponent("Color")
+		.color("#af0")
+		
+	setInterval(function(){
+		drawWorld(world.world,Crafty.canvas.context);
+	},1);
 }
